1756ed6d4c80d6becba78b6eafa7b3bf2be7eb1e
ilkaygunindi <90377712+ilkaygunindi@users.noreply.github.com>
2022-02-15 21:49:09 +0300

    Add project files.

---
+++ SayiTahmin2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiTahmin2
+{
+    class Program
+    {
+
+        static Random rastgele = new Random();
+
+        static void Main(string[] args)
+        {
+            int tahmin, sayi = rastgele.Next(100);
+            int can = 10;
+            
+            while (true)
+            {
+                Console.WriteLine("\n\n" + can-- + "canınız kaldı.");
+                if (can < 0)
+                {
+                    Console.WriteLine("Maalesef Kaybettiniz! Sayı : " + sayi);
+                    break;
+                }
+
+                Console.Write("Sayı Giriniz : ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if(tahmin > sayi)
+                {
+                    Console.WriteLine("Daha Küçük Bir Sayı Giriniz");
+                }
+                else if ( tahmin < sayi)
+                {
+                    Console.WriteLine("Daha Büyük Bir Sayı Giriniz");
+                }
+                else
+                {
+                    Console.WriteLine("Tebrikler!!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
